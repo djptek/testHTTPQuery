2cdd7c304e127d62ddf97634e2ad9cfc1252359a
djptek <dominic.page@elastic.co>
2020-03-24 15:25:50 +0000

    test on VM OK

--- testHTTPQuery.java
+++ testHTTPQuery.java
@@ -7,52 +7,51 @@ import java.net.URLEncoder;
 
 public class testHTTPQuery {
 
-	public static void main(String[] args) {
+    public static void callServlet(String sTargetHost) {
+        try {
+            URL url = new URL(sTargetHost);
 
+            HttpURLConnection http = (HttpURLConnection)url.openConnection();
 
-		String sTargetHost  = "http://127.0.0.1:8888/";
+            http.setDoInput(true);
+            http.setRequestMethod("GET");
 
-		try {
-			URL url = new URL(sTargetHost);
+            System.out.println("GET [" + sTargetHost + "]\n");
 
-			HttpURLConnection http = (HttpURLConnection)url.openConnection();
+            //Connect:
+            http.connect();
 
-			http.setDoInput(true);
-			http.setDoOutput(true);
-			http.setRequestMethod("GET");
-			//http.setConnectTimeout(1000);
-			//http.setReadTimeout(10000);
+            //Wait for answer.
+            BufferedReader in = new BufferedReader(new InputStreamReader(http.getInputStream()));
+            String inLine = null;
 
-			System.out.println("GET [" + sTargetHost + "]\n");
+            while((inLine = in.readLine()) != null) {
+                System.out.println(inLine + "\n");
+            }
+        }
+        catch( java.net.MalformedURLException e ) {
+            e.printStackTrace();
+        }
+        catch( java.io.IOException e ) {
+            e.printStackTrace();
+        }
+    }
 
-			//Connect:
-			http.connect();
+    public static void main(String[] args) {
 
-			//Send data to Stream:
-			OutputStream myOutputStream = null;
-			myOutputStream = http.getOutputStream ();
+        String sTargetHost  = "http://127.0.0.1:8080/";
+	int iSleepTimeMs = 10000;
 
-			//flush...
-			myOutputStream.flush();
-			myOutputStream.close();
-
-			//Wait for answer.
-			BufferedReader in = new BufferedReader(new InputStreamReader(http.getInputStream()));
-			String inLine = null;
-
-
-			while((inLine = in.readLine()) != null) {
-				System.out.println(inLine + "\n");
-			}
-
-		}
-		catch( java.net.MalformedURLException e ) {
-			e.printStackTrace();
-		}
-		catch( java.io.IOException e ) {
-			e.printStackTrace();
-		}
-	}
+	while (true) {
+            callServlet(sTargetHost);
+	    try {
+                Thread.sleep(iSleepTimeMs);
+	    }
+            catch( InterruptedException e ) {
+                System.out.println("Exiting...\n");
+	    }
+        }
+    }
 }
 
 
